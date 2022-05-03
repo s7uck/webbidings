ffca8dda2bd3662080afde6e42db5f1e15135afa
StuckDuck <stuck@stuckduck.it>
2022-05-03 22:30:21 +0200

    Update config.fish

--- linux/fish/config.fish
+++ linux/fish/config.fish
@@ -7,47 +7,73 @@ function l
     ll
 end
 
-function restart
-    exit
-    fish
+function drv
+    cd /mnt/$argv
 end
 
-# clear all the useless crap
-clear
+function windows
+    drv c
+end
+function apps
+    drv a
+end
+function files
+    drv q
+end
+function movies
+    drv y
+end
+
+set autosync false # /mnt/q/Proj/
+function autosync
+    for repo in $autosync/*/
+        if test -d $autosync/$repo/.git
+            git -C $repo pull
+        end
+    end
+end
+if $autosync
+    autosync
+end
+
+set moviesync "/mnt/y/∞ IQ"
+function moviesync
+    for playlist in $moviesync
+        set playlistName (string split -r -m1 / $playlist)[2]
+        if test $playlist/.metaData.db
+            cd $playlist/..
+            sync-dl sync -s "$playlistName"
+        end
+    end
+end
 
 # go home
 cd ~
 
 # spacing
 echo ''
-
-# spacing
+echo ''
+echo ''
 echo ''
 
 # delete history
 # history clear will prompt the user, so we use builtin here
+# (fap=Fish Activity Prune)
 function fap
-    # what? it stands for Fish Activity Prune
-    # >:(
     builtin history clear
 end
 
-# cap (clear + fap)
-function cap
-    clear
-    fap
-end
-
+set keep_history false
 # 'keep' command will tell fish not to clear history on logout for the current
 # session
-function keep
+function rkeep
     set keep_history true
 end
 
 # set the exit event handler
 function on_exit --on-process %self
-    echo "See you soon!"
-    if not keep_history
+    echo "See you soon!~"
+    if not $keep_history
         fap
     end
     set keep_history false
